1cb25eca3117bfe42b1faf43749200dbc9610bf0
ligen <ligen3@yusys.com.cn>
2021-09-22 16:03:32 +0800

    wxss_wxml_wxs

--- pages/scroll/scroll.js
+++
@@ -1,9 +0,0 @@
-// pages/scroll/scroll.js
-Page({
-  data: {
-
-  },
-  handleScroll(event) {
-    console.log('正在滚动', event)
-  }
-})
--- pages/wxs/wxs.js
+++ pages/wxs/wxs.js
@@ -1,11 +1,12 @@
-// pages/view/view.js
+// pages/wxs/wxs.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    price: "25.666666",
+    time: 1560598160
   },
 
   /**
